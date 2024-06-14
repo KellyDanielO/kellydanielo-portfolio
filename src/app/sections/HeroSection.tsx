0359b63625b5f23cf3a6b81facc07e80b3ac9a22
KellyDanielO <kizdver@gmail.com>
2024-06-14 11:15:12 +0100

    .

--- src/app/sections/HeroSection.tsx
+++ src/app/sections/HeroSection.tsx
@@ -1,7 +1,13 @@
+import { motion } from "framer-motion"
 import Link from "next/link"
 
 const HeroSection = () => {
-    return <section id="home-section" className="container-wrapper flex justify-center items-center flex-col py-20 lg:py-40 gap-2 lg:gap-5">
+    return <motion.section 
+    initial={{ opacity: 0, y: 50 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    viewport={{ once: false, amount: 0.25 }}
+    transition={{ duration: 0.5 }}
+     id="home-section" className="container-wrapper flex justify-center items-center flex-col py-20 lg:py-40 gap-2 lg:gap-5">
     <small className="text-primary font-freeman text-sm lg:text-2xl">Welcome!</small>
     <h1 className="text-2xl lg:text-8xl font-roboto font-bold text-center">Let's Transform Ideas Into <br />Digital Success</h1>
     <h2 className="text-lg lg:text-2xl text-greyColor text-center">I Build, Manage, and Elevate Your Digital Presence</h2>
@@ -10,7 +16,7 @@ const HeroSection = () => {
       <Link href={"#portfolio-section"} className="uppercase py-3 px-5 text-sm lg:text-lg bg-transparent border-2 font-freeman rounded-full" >My works</Link>
     </div>
 
-  </section>
+  </motion.section>
 }
 
 export default HeroSection
